d7cd77a4200a9f2d8d5804638bddff0ddb7a4997
Grzegorz Palian <greekomp@gmail.com>
2021-01-31 16:11:44 +0100

    Added more test cases

--- src/Wonder.test.ts
+++ src/Wonder.test.ts
@@ -1,8 +1,47 @@
-import { wonder } from "./Wonder";
+import { wonder, Wonder } from "./Wonder";
 
-test("Example test case", () => {
-  expect(wonder.GenerateLogElements(["hello", "world"])).toEqual([
-    "hello",
-    "world",
-  ]);
+type WonderTestCase = {
+  wonderInstance?: Wonder;
+  elements: any[];
+  expected: any[];
+};
+const wonderTestCases: WonderTestCase[] = [
+  {
+    elements: [],
+    expected: [],
+  },
+  {
+    elements: ["hello", "world"],
+    expected: ["hello", "world"],
+  },
+  {
+    elements: ["hello", wonder.red("world")],
+    expected: ["%chello%c %cworld", "", "", "color: red"],
+  },
+  {
+    elements: ["hello", wonder.red("world", wonder.blue("blue"))],
+    expected: [
+      "%chello%c %cworld%c %cblue",
+      "",
+      "",
+      "color: red",
+      "",
+      "color: blue",
+    ],
+  },
+  {
+    elements: [wonder.formatStringAsLiteral("literal string")],
+    expected: [
+      '%c"%c%cliteral string%c%c"',
+      "color: #F28B54",
+      "",
+      "color: #F28B54",
+      "",
+      "color: #F28B54",
+    ],
+  },
+];
+test.each(wonderTestCases)("wonder test case %#", (tc) => {
+  const w = tc.wonderInstance ?? wonder;
+  expect(w.GenerateLogElements(tc.elements)).toEqual(tc.expected);
 });
