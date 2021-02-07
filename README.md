c055a6d598226c78e67be58f7ed6ea86827cf652
Grzegorz Palian <greekomp@gmail.com>
2021-02-07 15:42:56 +0100

    Added doc comments for most properties, updated readme

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Wonderlog - simple and customizable browser console styling
 
+## About
+
+Wonderlog is a small library that aims to make styling console logs accessible.
+
+Browsers have supported applying styles to logs with the `%c` modifier for a while now, but it's too confusing and annoying to use.
+
+Wonderlog is inspired by [Chalk](https://github.com/chalk/chalk), but designed for use with browser consoles instead of the terminal.
+
 ## Highlights
 
-- easy to use, intuitive ui
+- easy to use, intuitive interface
 - fully customizable
-- usefull predefined styles included
+- useful predefined styles included
+- dependency-free
 - typescript support
 
 ## Installation
@@ -13,9 +22,7 @@
 npm install wonderlog-console
 ```
 
-## Usage
-
-Basic usage:
+# Basic usage
 
 ```js
 import { wonder } from "wonderlog-console";
@@ -27,7 +34,9 @@ wonder.error("Error");
 // + group, groupCollapsed, groupEnd, debug
 ```
 
-Using styles:
+![Basic usage example screen 01](docs/img/NormalLogWarningError.png)
+
+### Using styles
 
 ```js
 // ✨ Apply styles using the predefined properties
@@ -40,7 +49,9 @@ wonder.log(wonder.color("#FF00FF")("Magenta text"));
 wonder.log(wonder.bg("black").white("White text on a black background"));
 ```
 
-Nesting styles:
+![Basic usage example screen 02](docs/img/UsingStyles_01.png)
+
+### Nesting styles
 
 ```js
 // Styles can be nested and chained intuitively 🎒
@@ -50,25 +61,98 @@ wonder.log(wonder.red("Red text", wonder.bold("Bold red text", wonder.blue("Bold
 wonder.log(wonder.red.green.blue("Blue text"));
 ```
 
-## Readme WIP
+### Saving presets
 
-<!-- ## Formatting data
+```js
+// Styles can be saved and reused easily
+const success = wonder.green.pre("✅");
+const failure = wonder.red.pre("❌");
 
-By default, wonder passes data to the console regardless of it's type:
+wonder.log("Styling", success("successful"), "iritation", failure("not found"));
+```
+
+![Saving presets example screen](docs/img/StylingSuccessfulIritationNotFound.png)
+
+# Advanced usage
+
+## Formatting data
+
+By default, wonder passes data to the console directly, regardless of its type:
 
 ```js
 wonder.log(["this", "array", "has", 5, "elements"]);
-// ➡ console.log(["this", "array", "has", 5, "elements"])
+// Results in: console.log(["this", "array", "has", 5, "elements"])
 wonder.log({ exampleProperty: "example property value" });
-// ➡ console.log({ exampleProperty: "example property value" })
+// Results in: console.log({ exampleProperty: "example property value" })
 ```
 
-However, if you apply any styles to the data, it will have to be converted into strings so that it can be styled in the console. By default wonder uses `toString()` to get the displayed value, but you can override this behaviour using formatters:
+However, if you apply any styles to the data, it will have to be converted into strings so that it can be styled in the console.
+
+By default wonder uses `toString()` to get the displayed value, but you can override this behaviour using formatters:
 
 ```js
-// Default behaviour:
+// Default behaviour
 wonder.log(wonder.red({ exampleProperty: "example property value" }));
+// 📢 [object Object]
 
-//
+// Converting objects to JSON 🎈
+wonder.log(
+  wonder.red.formatObjectAsJson({ exampleProperty: "example property value" })
+);
+// 📢 {"exampleProperty":"example property value"}
+wonder.log(
+  wonder.red.formatObjectAsMultilineJson({
+    exampleProperty: "example property value",
+  })
+);
+```
 
-``` -->
+### Other formatters
+
+```js
+wonder("one", "two", "three");
+// 📢 one two three
+wonder.formatStringAsLiteral("one", "two", "three");
+// 📢 "one" "two" "three"
+
+wonder(1234567.890123);
+// 📢 1234567.890123
+wonder.formatNumberAsLiteral(1234567.890123);
+wonder.formatNumberAs(new Intl.NumberFormat("en-US"))(1234567.890123);
+// 📢 1,234,567.89  💡 uses `Intl.NumberFormat("en-US")` by default
+
+wonder(["one", "two", 3.0]);
+// 📢 one,two,3  💡 just like ["one", "two", 3.0].toString()
+wonder.formatArrayAsIndividualItems(["one", "two", 3.0]);
+// 📢 [one, two, 3]
+wonder.formatArrayAsIndividualItems.formatStringAsLiteral(["one", "two", 3.0]);
+// 📢 ["one", "two", 3]
+```
+
+### Note - browser limitation
+
+Unfortunately browsers don't support interweaving styled string and objects within a single console.log.
+
+However, it is possible to get both in a single line, by logging any objects after the styled content:
+
+```js
+// This might give unexpected results
+wonder.log("Text", { foo: "bar" }, wonder.red("red text"));
+// 📢 Text [object Object] red text
+
+// But this should work correctly
+wonder.log("Text", wonder.red("red text"), { foo: "bar" });
+// 📢 Text red text {foo: "bar"}
+```
+
+💡 This is not a limitation of `wonderlog`, but a result of how applying styles in the browser console works.
+
+## Showing correct line numbers
+
+You can configure your developer tools to show the correct line numbers by **blackboxing** the `WonderImplementation.js` file.
+
+[See chrome devTools docs](https://developers.google.com/web/tools/chrome-devtools/javascript/reference#blackbox)
+
+## Feedback
+
+If you have any feedback or suggestions for this package, feel free to open an issue on [Github](https://github.com/grekomp/wonderlog).
--- src/WonderImplementation.ts
+++ src/WonderImplementation.ts
@@ -32,33 +32,73 @@ export default class WonderImplementation {
   }
 
   //#region Logging methods
+  /**
+   * Logs `entries` to the browser console using `console.log`, applying styles from any wonderlog entries.
+   */
   log(...entries: LogEntry[]) {
     console.log(...WonderHelper.GenerateLogElements(this, entries));
   }
+  /**
+   * Logs `entries` to the browser console using `console.debug`, applying styles from any wonderlog entries.
+   */
   debug(...entries: LogEntry[]): void {
     console.debug(...WonderHelper.GenerateLogElements(this, entries));
   }
+  /**
+   * Logs `entries` to the browser console using `console.warn`, applying styles from any wonderlog entries.
+   */
   warn(...entries: LogEntry[]): void {
     console.warn(...WonderHelper.GenerateLogElements(this, entries));
   }
+  /**
+   * Logs `entries` to the browser console using `console.error`, applying styles from any wonderlog entries.
+   */
   error(...entries: LogEntry[]): void {
     console.error(...WonderHelper.GenerateLogElements(this, entries));
   }
+  /**
+   * Logs `entries` to the browser console using `console.group`, applying styles from any wonderlog entries.
+   * Increases the indent of any following logs, until you close the group with `wonder.groupEnd`
+   */
   group(...entries: LogEntry[]): void {
     console.group(...WonderHelper.GenerateLogElements(this, entries));
   }
+  /**
+   * Logs `entries` to the browser console using `console.groupCollapsed`, applying styles from any wonderlog entries.
+   * Increases the indent of any following logs, until you close the group with `wonder.groupEnd`.
+   * The following entries are hidden by default, until the group is expanded in the developer tools.
+   */
   groupCollapsed(...entries: LogEntry[]): void {
     console.groupCollapsed(...WonderHelper.GenerateLogElements(this, entries));
   }
+  /**
+   * Closes last log group and decreases the indent level
+   */
   groupEnd(): void {
     console.groupEnd();
   }
   //#endregion Logging methods
   //#region Setting content
+  /**
+   * Sets the content to be logged.
+   * Wonderlog instances can also be called as a function for a shorthand of this method.
+   *
+   * ```js
+   * wonder.log(wonder.red._apply("red text"));
+   * wonder.log(wonder.red("red text"));
+   * ```
+   */
   _apply(content: LogEntry[]): Wonder {
     return WonderHelper.create(this, { content });
   }
 
+  /**
+   * Adds a prefix to the content of this entry.
+   *
+   * ```js
+   * wonder.log(wonder.pre("👉")("look at me!")) // 👉look at me!
+   * ```
+   */
   pre(prefixValue: WonderOptions | string): Wonder {
     if (typeof prefixValue === "string") {
       return WonderHelper.create(this, {
@@ -68,6 +108,13 @@ export default class WonderImplementation {
 
     return WonderHelper.create(this, { prefixValue });
   }
+  /**
+   * Adds a postfix to the content of this entry.
+   *
+   * ```js
+   * wonder.log(wonder.post("👈")("look at me!")) // look at me!👈
+   * ```
+   */
   post(postfixValue: WonderOptions | string): Wonder {
     if (typeof postfixValue === "string") {
       return WonderHelper.create(this, {
@@ -78,90 +125,197 @@ export default class WonderImplementation {
     return WonderHelper.create(this, { postfixValue });
   }
 
+  /**
+   * Sets the inner separator to be used between inner elements of this log entry.
+   * Defaults to one space: " "
+   *
+   * ```js
+   * wonder.log(wonder.separator("+")("first", "second")) // first+second
+   * ```
+   */
   separator(separatorString: string): Wonder {
     return WonderHelper.create(this, {
       innerSeparator: separatorString,
     });
   }
+  /**
+   * Sets the separator to be used between the prefix (set by `pre`) and the contents of this entry.
+   * Only used when there is a prefix.
+   *
+   * ```js
+   * wonder.log(wonder.preSeparator(": ").pre("Simon says")("Hello world!"))
+   * // Simon says: Hello world!
+   * ```
+   */
   preSeparator(prefixSeparator: string): Wonder {
     return WonderHelper.create(this, { prefixSeparator });
   }
+  /**
+   * Sets the separator to be used between the postfix (set by `post`) and the contents of this entry.
+   * Only used when there is a postfix.
+   *
+   * ```js
+   * wonder.log(wonder.postSeparator(" - ").post("said Simon")("Hello world!"))
+   * // Hello world! - said Simon
+   * ```
+   */
   postSeparator(postfixSeparator: string): Wonder {
     return WonderHelper.create(this, { postfixSeparator });
   }
+  /**
+   * An alias for `separator("")`.
+   */
   get noSeparator(): Wonder {
     return this.separator("");
   }
   //#endregion Setting content
   //#region Setting styles
+  /**
+   * Sets the text color. The color can be any color string format accepted by the browser in css.
+   *
+   * In most cases you should use the predefined colors, like `wonder.red`, `wonder.green` etc.
+   *
+   * ```js
+   * wonder.log(wonder.color("hsl(271, 100%, 71%)")("purple text"));
+   * ```
+   */
   color(color: string): Wonder {
     return WonderHelper.create(this, {
       style: { color },
     });
   }
+  /**
+   * Sets text color. An alias for `wonder.color`.
+   */
   get red(): Wonder {
     return this.color(colors.red);
   }
+  /**
+   * Sets text color. An alias for `wonder.color`.
+   */
   get green(): Wonder {
     return this.color(colors.green);
   }
+  /**
+   * Sets text color. An alias for `wonder.color`.
+   */
   get blue(): Wonder {
     return this.color(colors.blue);
   }
+  /**
+   * Sets text color. An alias for `wonder.color`.
+   */
   get orange(): Wonder {
     return this.color(colors.orange);
   }
+  /**
+   * Sets text color. An alias for `wonder.color`.
+   */
   get yellow(): Wonder {
     return this.color(colors.yellow);
   }
+  /**
+   * Sets text color. An alias for `wonder.color`.
+   */
   get turquoise(): Wonder {
     return this.color(colors.turquoise);
   }
+  /**
+   * Sets text color. An alias for `wonder.color`.
+   */
   get cyan(): Wonder {
     return this.color(colors.cyan);
   }
+  /**
+   * Sets text color. An alias for `wonder.color`.
+   */
   get purple(): Wonder {
     return this.color(colors.purple);
   }
+  /**
+   * Sets text color. An alias for `wonder.color`.
+   */
   get black(): Wonder {
     return this.color(colors.black);
   }
+  /**
+   * Sets text color. An alias for `wonder.color`.
+   */
   get white(): Wonder {
     return this.color(colors.white);
   }
 
+  /**
+   * Sets the background color. The color can be any color string format accepted by the browser in css.
+   *
+   * In most cases you should use the predefined colors, like `wonder.bgRed`, `wonder.bgGreen` etc.
+   *
+   * ```js
+   * wonder.log(wonder.bg("hsl(271, 100%, 71%)")("Text on a purple background"));
+   * ```
+   */
   bg(bgColor: string): Wonder {
     return WonderHelper.create(this, {
       style: { background: bgColor },
     });
   }
+  /**
+   * Sets the background color. An alias for `wonder.bg`.
+   */
   get bgOrange(): Wonder {
     return this.bg(colors.orange);
   }
+  /**
+   * Sets the background color. An alias for `wonder.bg`.
+   */
   get bgYellow(): Wonder {
     return this.bg(colors.yellow);
   }
+  /**
+   * Sets the background color. An alias for `wonder.bg`.
+   */
   get bgGreen(): Wonder {
     return this.bg(colors.green);
   }
+  /**
+   * Sets the background color. An alias for `wonder.bg`.
+   */
   get bgTurquoise(): Wonder {
     return this.bg(colors.turquoise);
   }
+  /**
+   * Sets the background color. An alias for `wonder.bg`.
+   */
   get bgCyan(): Wonder {
     return this.bg(colors.cyan);
   }
+  /**
+   * Sets the background color. An alias for `wonder.bg`.
+   */
   get bgBlue(): Wonder {
     return this.bg(colors.blue);
   }
+  /**
+   * Sets the background color. An alias for `wonder.bg`.
+   */
   get bgPurple(): Wonder {
     return this.bg(colors.purple);
   }
+  /**
+   * Sets the background color. An alias for `wonder.bg`.
+   */
   get bgRed(): Wonder {
     return this.bg(colors.red);
   }
+  /**
+   * Sets the background color. An alias for `wonder.bg`.
+   */
   get bgBlack(): Wonder {
     return this.bg(colors.black);
   }
+  /**
+   * Sets the background color. An alias for `wonder.bg`.
+   */
   get bgWhite(): Wonder {
     return this.bg(colors.white);
   }
